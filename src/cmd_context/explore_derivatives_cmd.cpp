04ea9a70808c832eef34b1bbc63912f0eca01373
Caleb Stanford <caleb.pirsquared@gmail.com>
2020-09-14 20:38:58 -0400

    prune explore-derivatives placeholder code

--- src/cmd_context/explore_derivatives_cmd.cpp
+++ src/cmd_context/explore_derivatives_cmd.cpp
@@ -20,28 +20,29 @@ class explore_derivatives_cmd : public parametric_cmd {
 
     expr *                   m_target;
 public:
-    explore_derivatives_cmd(char const * name = "explore_derivatives"):parametric_cmd(name) {}
+    explore_derivatives_cmd(
+        char const * name = "explore_derivatives"
+    ) : parametric_cmd(name) {}
 
-    char const * get_usage() const override { return "<term> (<keyword> <value>)*"; }
+    ~explore_derivatives_cmd() override {}
+
+    char const * get_usage() const override { return "<term>"; }
 
     char const * get_main_descr() const override {
         return "explore all derivatives of the given term, which must be a regular expression.";
     }
 
-    void init_pdescrs(cmd_context & ctx, param_descrs & p) override {
-        th_rewriter::get_param_descrs(p);
-        insert_timeout(p);
-        p.insert("print", CPK_BOOL, "(default: true)  print the simplified term.");
-        p.insert("print_proofs", CPK_BOOL, "(default: false) print a proof showing the original term is equal to the resultant one.");
-        p.insert("print_statistics", CPK_BOOL, "(default: false) print statistics.");
-    }
-
-    ~explore_derivatives_cmd() override {
-    }
-
     void prepare(cmd_context & ctx) override {
         parametric_cmd::prepare(ctx);
         m_target   = nullptr;
+    }
+
+    void init_pdescrs(cmd_context & ctx, param_descrs & p) override {
+        th_rewriter::get_param_descrs(p);
+        insert_timeout(p);
+        // p.insert("print", CPK_BOOL, "(default: true)  print the simplified term.");
+        // p.insert("print_proofs", CPK_BOOL, "(default: false) print a proof showing the original term is equal to the resultant one.");
+        // p.insert("print_statistics", CPK_BOOL, "(default: false) print statistics.");
     }
 
     cmd_arg_kind next_arg_kind(cmd_context & ctx) const override {
@@ -58,16 +59,16 @@ public:
             throw cmd_exception("invalid explore_derivatives command, argument expected");
         expr_ref r(ctx.m());
         proof_ref pr(ctx.m());
-        if (m_params.get_bool("som", false))
-            m_params.set_bool("flat", true);
+        // if (m_params.get_bool("som", false))
+        //     m_params.set_bool("flat", true);
         th_rewriter s(ctx.m(), m_params);
         th_solver solver(ctx);
         s.set_solver(alloc(th_solver, ctx));
-        unsigned cache_sz;
-        unsigned num_steps = 0;
+        // unsigned cache_sz;
+        // unsigned num_steps = 0;
         unsigned timeout   = m_params.get_uint("timeout", UINT_MAX);
         unsigned rlimit    = m_params.get_uint("rlimit", UINT_MAX);
-        bool failed = false;
+        // bool failed = false;
         cancel_eh<reslimit> eh(ctx.m().limit());
         {
             scoped_rlimit _rlimit(ctx.m().limit(), rlimit);
@@ -82,38 +83,38 @@ public:
             }
             catch (z3_exception & ex) {
                 ctx.regular_stream() << "(error \"simplifier failed: " << ex.msg() << "\")" << std::endl;
-                failed = true;
+                // failed = true;
                 r = m_target;
             }
-            cache_sz  = s.get_cache_size();
-            num_steps = s.get_num_steps();
+            // cache_sz  = s.get_cache_size();
+            // num_steps = s.get_num_steps();
             s.cleanup();
         }
-        if (m_params.get_bool("print", true)) {
-            ctx.display(ctx.regular_stream(), r);
-            ctx.regular_stream() << std::endl;
-        }
-        if (!failed && m_params.get_bool("print_proofs", false)) {
-            ast_smt_pp pp(ctx.m());
-            pp.set_logic(ctx.get_logic());
-            pp.display_expr_smt2(ctx.regular_stream(), pr.get());
-            ctx.regular_stream() << std::endl;
-        }
-        if (m_params.get_bool("print_statistics", false)) {
-            shared_occs s1(ctx.m());
-            if (!failed)
-                s1(r);
-            unsigned long long max_mem = memory::get_max_used_memory();
-            unsigned long long mem = memory::get_allocation_size();
-            ctx.regular_stream() << "(:time " << std::fixed << std::setprecision(2) << ctx.get_seconds() << " :num-steps " << num_steps
-                                 << " :memory " << std::fixed << std::setprecision(2) << static_cast<double>(mem)/static_cast<double>(1024*1024)
-                                 << " :max-memory " << std::fixed << std::setprecision(2) << static_cast<double>(max_mem)/static_cast<double>(1024*1024)
-                                 << " :cache-size: " << cache_sz
-                                 << " :num-nodes-before " << get_num_exprs(m_target);
-            if (!failed)
-                ctx.regular_stream() << " :num-shared " << s1.num_shared() << " :num-nodes " << get_num_exprs(r);
-            ctx.regular_stream() << ")" << std::endl;
-        }
+        // if (m_params.get_bool("print", true)) {
+        ctx.display(ctx.regular_stream(), r);
+        ctx.regular_stream() << std::endl;
+        // }
+        // if (!failed && m_params.get_bool("print_proofs", false)) {
+        //     ast_smt_pp pp(ctx.m());
+        //     pp.set_logic(ctx.get_logic());
+        //     pp.display_expr_smt2(ctx.regular_stream(), pr.get());
+        //     ctx.regular_stream() << std::endl;
+        // }
+        // if (m_params.get_bool("print_statistics", false)) {
+        //     shared_occs s1(ctx.m());
+        //     if (!failed)
+        //         s1(r);
+        //     unsigned long long max_mem = memory::get_max_used_memory();
+        //     unsigned long long mem = memory::get_allocation_size();
+        //     ctx.regular_stream() << "(:time " << std::fixed << std::setprecision(2) << ctx.get_seconds() << " :num-steps " << num_steps
+        //                          << " :memory " << std::fixed << std::setprecision(2) << static_cast<double>(mem)/static_cast<double>(1024*1024)
+        //                          << " :max-memory " << std::fixed << std::setprecision(2) << static_cast<double>(max_mem)/static_cast<double>(1024*1024)
+        //                          << " :cache-size: " << cache_sz
+        //                          << " :num-nodes-before " << get_num_exprs(m_target);
+        //     if (!failed)
+        //         ctx.regular_stream() << " :num-shared " << s1.num_shared() << " :num-nodes " << get_num_exprs(r);
+        //     ctx.regular_stream() << ")" << std::endl;
+        // }
     }
 };
 
