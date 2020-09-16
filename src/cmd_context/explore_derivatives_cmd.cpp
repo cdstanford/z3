6069b9a724b7efdb60c3f871da1279c4dd201e0c
Caleb Stanford <caleb.pirsquared@gmail.com>
2020-09-16 17:33:26 -0400

    fix and cleanup explore_derivatives command

--- src/cmd_context/explore_derivatives_cmd.cpp
+++ src/cmd_context/explore_derivatives_cmd.cpp
@@ -13,7 +13,7 @@ of a regex.
 #include "util/scoped_timer.h"
 #include "util/scoped_ctrl_c.h"
 #include "util/cancel_eh.h"
-#include<iomanip>
+#include <iomanip>
 
 #include "smt/smt_context.h"
 #include "ast/reg_decl_plugins.h"
@@ -21,9 +21,7 @@ of a regex.
 #include "smt/seq_regex.h"
 
 class explore_derivatives_cmd : public parametric_cmd {
-
-
-    expr *                   m_target;
+    expr *m_target;
 public:
     explore_derivatives_cmd(
         char const * name = "explore_derivatives"
@@ -45,9 +43,6 @@ public:
     void init_pdescrs(cmd_context & ctx, param_descrs & p) override {
         th_rewriter::get_param_descrs(p);
         insert_timeout(p);
-        // p.insert("print", CPK_BOOL, "(default: true)  print the simplified term.");
-        // p.insert("print_proofs", CPK_BOOL, "(default: false) print a proof showing the original term is equal to the resultant one.");
-        // p.insert("print_statistics", CPK_BOOL, "(default: false) print statistics.");
     }
 
     cmd_arg_kind next_arg_kind(cmd_context & ctx) const override {
@@ -62,75 +57,39 @@ public:
     void execute(cmd_context & ctx) override {
         if (m_target == nullptr)
             throw cmd_exception("invalid explore_derivatives command, argument expected");
-        expr_ref r(ctx.m());
-        proof_ref pr(ctx.m());
 
-        // if (m_params.get_bool("som", false))
-        //     m_params.set_bool("flat", true);
-        th_rewriter s(ctx.m(), m_params);
-        th_solver solver(ctx);
-        s.set_solver(alloc(th_solver, ctx));
-        // unsigned cache_sz;
-        // unsigned num_steps = 0;
-        unsigned timeout   = m_params.get_uint("timeout", UINT_MAX);
-        unsigned rlimit    = m_params.get_uint("rlimit", UINT_MAX);
-        // bool failed = false;
-        cancel_eh<reslimit> eh(ctx.m().limit());
-        {
-            scoped_rlimit _rlimit(ctx.m().limit(), rlimit);
-            scoped_ctrl_c ctrlc(eh);
-            scoped_timer timer(timeout, &eh);
-            cmd_context::scoped_watch sw(ctx);
-            try {
-                s(m_target, r, pr);
-            }
-            catch (z3_error & ex) {
-                throw ex;
-            }
-            catch (z3_exception & ex) {
-                ctx.regular_stream() << "(error \"simplifier failed: " << ex.msg() << "\")" << std::endl;
-                // failed = true;
-                r = m_target;
-            }
-            // cache_sz  = s.get_cache_size();
-            // num_steps = s.get_num_steps();
-            s.cleanup();
-        }
-        // if (m_params.get_bool("print", true)) {
-        ctx.display(ctx.regular_stream(), r);
-        ctx.regular_stream() << std::endl;
-        // }
-        // if (!failed && m_params.get_bool("print_proofs", false)) {
-        //     ast_smt_pp pp(ctx.m());
-        //     pp.set_logic(ctx.get_logic());
-        //     pp.display_expr_smt2(ctx.regular_stream(), pr.get());
-        //     ctx.regular_stream() << std::endl;
-        // }
-        // if (m_params.get_bool("print_statistics", false)) {
-        //     shared_occs s1(ctx.m());
-        //     if (!failed)
-        //         s1(r);
-        //     unsigned long long max_mem = memory::get_max_used_memory();
-        //     unsigned long long mem = memory::get_allocation_size();
-        //     ctx.regular_stream() << "(:time " << std::fixed << std::setprecision(2) << ctx.get_seconds() << " :num-steps " << num_steps
-        //                          << " :memory " << std::fixed << std::setprecision(2) << static_cast<double>(mem)/static_cast<double>(1024*1024)
-        //                          << " :max-memory " << std::fixed << std::setprecision(2) << static_cast<double>(max_mem)/static_cast<double>(1024*1024)
-        //                          << " :cache-size: " << cache_sz
-        //                          << " :num-nodes-before " << get_num_exprs(m_target);
-        //     if (!failed)
-        //         ctx.regular_stream() << " :num-shared " << s1.num_shared() << " :num-nodes " << get_num_exprs(r);
-        //     ctx.regular_stream() << ")" << std::endl;
-        // }
+        // 1. Initialize rewriter
+        th_rewriter th_rw(ctx.m(), m_params);
+        th_rw.set_solver(alloc(th_solver, ctx));
 
-        // Get SMT handle to seq_regex
-        smt_params params;
-        ast_manager m;
-        reg_decl_plugins(m);
-        smt::context sctx(m, params);
-        smt::theory_seq th_seq(sctx);
+        // 2. Initialize SMT handle to seq_regex
+        smt_params th_seq_params;
+        smt::context th_seq_ctx(ctx.m(), th_seq_params);
+        smt::theory_seq th_seq(th_seq_ctx);
         smt::seq_regex th_regex(th_seq);
 
-        // Explore all derivatives of r
+        // 3. Rewrite the expression
+        expr_ref r(ctx.m());
+        proof_ref _pr(ctx.m());
+        try {
+            th_rw(m_target, r, _pr);
+        }
+        catch (z3_error & ex) {
+            throw ex;
+        }
+        catch (z3_exception & ex) {
+            ctx.regular_stream()
+                << "(error \"simplifier failed: "
+                << ex.msg() << "\")" << std::endl;
+            r = m_target;
+        }
+        th_rw.cleanup();
+
+        // For debugging: display rewritten expression
+        // ctx.display(ctx.regular_stream(), r);
+        // ctx.regular_stream() << std::endl;
+
+        // 4. Explore all derivatives of r
         th_regex.public_explore_all_derivs(r);
 
     }
